4af2aacfece66718df01413e9bb44b79b484c0c4
siangry <168882131+siangry@users.noreply.github.com>
2024-08-16 18:07:39 +0800

    Add files via upload

---
+++ MakanMakanMalaysia/recipe-api/server.js
@@ -0,0 +1,345 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// In-memory recipe database
+let recipes = [
+    {
+        id: 1,
+        title: "Beef Short Rib Rendang Recipe",
+        author: "Chichi Wang",
+        category: "main",
+        image: url="https://www.seriouseats.com/thmb/ZnIVrwfBh7TW2cuFeuldRCqQCX8=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__recipes__images__20100402-seasian-beefrendang-001efd0103804fb6b1b07b8ab1cae331.jpg",
+        ingredients: [ 
+            "5-600g Beef for stewing, chuck or short ribs",
+            "2 Lemongrass","1 Cinnamon",
+            "3 Star Anise",
+            "5 Cloves",
+            "5 Cardamom",
+            "5-6 Kaffir Lime Leaves",
+            "1 tsp Tamarind Paste + 2 tbsp Water",
+            "500ml Coconut Milk or Santan",
+            "1 tbsp Coconut Palm Sugar",
+            "1 tbsp Salt",
+            "120g Toasted Coconut Butter or Kerisik",
+            "1 cup water",
+            "2 pandan leaves",
+            "1/4 tsp salt",
+        ],
+        instructions: [
+            "Wash and cut the beef",
+            "Bruise lemongrass",
+            "01:30 - How to prep dried chillies",
+            "Blending up rempah spice base paste",
+            "Cook the rendang!",
+            "Adding the beef"
+            
+        ],
+        youtubeId: "https://www.youtube.com/embed/sMxkA5IMdaQ" 
+    },
+    {
+        id: 2,
+        title: "Indian Chicken Curry Recipe",
+        author: "Debjani Chatterjee Alam",
+        category: "main",
+        image: url='https://kitchenofdebjani.com/wp-content/uploads/2023/04/easy-indian-chicken-curry-Recipe-for-beginners-Debjanir-rannaghar-1024x1024.jpg',
+        ingredients:["Chicken- 1 kg",
+        "Onion, rough chopped- 4 medium",
+        "Ginger, crushed- 2 tsp",
+        "Garlic, crushed- 2 tsp",
+        "Tomatoes, roughly chopped- 3 medium",
+        "Green chillies, thick chopped- 3 nos",
+        "Turmeric powder- 1/2 tsp",
+        "Red Chilli Powder- 1.5 tsp",
+        "Readymade Chicken Masala- 1 tbsp (store bought)",
+        "Salt- 1.5 tsp",
+        "Refined Oil- 4-5 tbsp "
+        ],
+        instructions:["Clean & wash the chicken pieces and keep it to drain",
+        "Roughly chop the onions, tomatoes and green chillies",
+        "Peel and crush the ginger and garlic cloves",
+        "Heat oil in a pan or kadai and add the chopped onions",
+        "Add the crushed ginger,garlic and chopped tomatoes and green chilies",
+        "Add the turmeric powder and mix & fry on medium heat for around 3 mins till tomatoes are soft",
+        "Now add the chicken pieces and fry on high heat for 2-3 mins till the chicken pieces are browned",
+        "Add the readymade chicken masala, red Chilli powder and salt, mix and bhunno on high heat for 5 mins. Continue to fry on medium heat for another 5 mins",
+        "Now add 200 ml water and cook on low heat for around 20 mins till the chicken pieces are tender and oil separates"
+    ],
+    youtubeId: "https://www.youtube.com/embed/x3Mzind-TBo"
+    },
+    {
+        id: 3,
+        title: "Egg Fried Rice",
+        author: "Elle",
+        category: "main",
+        image: url='https://www.cookerru.com/wp-content/uploads/2022/07/egg-fried-rice-main-preview.jpg',
+        ingredients:["Rice, boiled -250 to 300 gms",
+        "Eggs - 3 nos ",
+        "Garlic fine chopped - 5-6 nos",
+        "Spring onions/ scallions- 3 nos",
+        "Carrot, fine diced - 1/2 cup",
+        "Beans, fine sliced- 1/2 cup",
+        "Salt, White pepper, Soy sauce, Peanut oil"
+        ],
+        instructions:["Heat 1 tbsp of peanut oil. Now break 3 eggs side by side and sprinkle salt & pepper. Also sprinkle some of the green spring onions/ scallions",
+        "Now gently fold it in & scramble the eggs lightly ",
+        "Remove the eggs onto a plate & set aside",
+        "Add one more tablespoon of peanut oil & heat it",
+        "Add the white portion of the spring onions, fine chopped garlic,carrots and sliced beans",
+        "Continue to mix & sauté and add the rice together",
+        "Mix it well"
+    ],
+    youtubeId: "https://www.youtube.com/embed/GfyJS-flQQo"
+    },
+    {
+        id: 4,
+        title: "Pengat Pisang ",
+        author: "Alia Ali",
+        category: "sides",
+        image: url='https://www.seriouseats.com/thmb/VHGFZtDQqfQPj0Ca2sQ5mwj-q2s=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20240227-SEA-PengatPisang-MichelleYip-00-e7a664f8356741918db61517cc579a4a.jpg',
+        ingredients:["100g dried sago pearls,under-ripe bananas, peeled and sliced into 2-inch pieces",
+        "1/3 cup palm sugar (78g)",
+        "1/4 teaspoon Diamond Crystal kosher salt",
+        "2 pandan leaves (9g), fresh or frozen",
+        "3 cups (720ml) water, plus more for soaking the sago pearls",
+        "2/3 cups (400ml) fresh coconut milk or one 14.5 ounce can full-fat coconut milk, stirred or shaken well"
+        ],
+        instructions:["In a medium saucepan, cover sliced bananas, palm sugar, salt, and pandan leaves with the 3 cups (720ml) water. Bring to a boil over high heat, then reduce heat to medium-low and continue cooking, stirring occasionally, until the bananas begin to soften, about 10 minutes.",
+        "Add (washed) sago pearls into the saucepan and keep stiring it ",
+        "Add the coconut milk and cook, stirring, until bubbles begin to appear around the edges, about 2 minutes. Turn off the heat and allow pengat to sit until it has cooled slightly, about 5 minutes. "
+        ],
+    youtubeId: "https://www.youtube.com/embed/lou7oadLmXo"
+    },
+    {
+        id: 5,
+        title: "Kaya Toast",
+        author: "KP Kwan",
+        category: "sides",
+        image: "https://www.seriouseats.com/thmb/8XoWNQ0LLRClerm6Z9Uo__AxJwM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/20240223-SEA-KayaToast-MichelleYip-04hero-d7da174497f64fd196ef1accb85b5e8a.jpg",
+        ingredients:["2 slices of thick toast",
+        "1 tbsp kaya",
+        "60g butter",
+        ],
+        instructions:["Cut the bread into thick slices, between half to one inch thick.",
+        "Grill the bread on open charcoal fire or with a grill pan over medium to high heat.",
+        "When the first side of the bread has developed the grill mark, turn it over to continue grilling the other side.",
+        "Remove from heat and apply the kaya generously on one of the toast.",
+        "Sandwich a slab of ice-cold butter in between the toast to enjoy.",
+    ],
+    youtubeId: "https://www.youtube.com/embed/Gn0kV6ZkIXA"
+    },
+    {
+        id: 6,
+        title: "Malaysian ABC Soup",
+        author: "Susanna",
+        category: "soup",
+        image: url='https://smellylunchbox.com/wp-content/uploads/2023/09/abcsoup-1-750x500.jpg',
+        ingredients:["450-500 gram Chicken drumsticks Or chicken with bones ( About 15 oz )",
+        "1 Potato Cut big cubes",
+        "1 Carrot Cut big cubes",
+        "2 Tomatoes Cut wedges",
+        "1 Onion Cut big cubes",
+        "1 thumb Ginger Cut thin slices",
+        "1 stick Celery Cut thin slices",
+        "Salt, Black pepper, Spring Onion and water",
+        ],
+        instructions:["Boil the water in large pot. Add the chicken pieces and blanch them in the boiling water.",
+        "Drain the water out and rinse the chicken with cold water.",
+        "Place the chicken drumsticks back in the pot.",
+        "Add the vegetables, celery and ginger slices. Pour the water in and bring it to boil.  ",
+        "Add salt to taste.Add the tomatoes and cook for another 15 minutes.",
+        "Add freshly ground black pepper and sprinkle with chopped spring onions.",
+    ],
+    youtubeId: "https://www.youtube.com/embed/EjOeqJUIB3U"
+    
+    },
+    {
+        id: 7,
+        title: "Roti Canai",
+        author: "Bee Yinn Low",
+        category: "main",
+        image: url='https://rasamalaysia.com/wp-content/uploads/2010/03/roti-canai-roti-paratha-roti-prata.jpg',
+        ingredients:["300mL water, lukewarm (plus up to 3 tsp more water)",
+        "60g (3 tbsp) condensed milk (sweetened)",
+        "35g (2-1/2 tbsp) melted butter OR ghee",
+        "12g (2 tsp) fine sea salt OR table salt",
+        "20g (1-1/2 tbsp) sugar",
+        "600g (4-1/4 cups) all-purpose flour"
+        ],
+        instructions:["Add water, condensed milk, butter or ghee, salt, and sugar to a large mixing bowl. Stir together until thoroughly combined.",
+        "Add flour and stir with a spatula then switch to hands and knead to bring the dough together. If there’s excess dry flour, add up to 3 tsp more water. Knead only until all the dry flour is incorporated and the dough is free of any hard lumps. It’ll still be quite rough in texture.",
+        "Cover and rest 15 minutes, Knead dough until smooth,place tray in fridge overnight. The next day, leave the tray at room temperature for 1 hour before proceeding.",
+        "Stretch each dough ball into a large and translucently thin sheet",
+        "place in roti. Cook until the bottom is golden brown. If roti puffs, press down lightly with spatula to encourage even cooking. Once the bottom is golden, flip over and cook other side. If using ghee, cook until both sides are golden and transfer out. If using oil, when the second side is lightly golden, add to the pan a knob of butter (1-2 tbsp). Swirl around to melt and coat both sides of the roti.",
+        "Continue cooking until golden and transfer out of pan."
+         ],
+    youtubeId:"https://www.youtube.com/embed/qEuk50GIofE"
+    
+    },
+    {
+        id: 8,
+        title: "Claypot Chicken Rice",
+        author: "Cherry",
+        category: "main",
+        image: url='https://makan.ch/wp-content/uploads/2015/08/claypotrice.jpg.webp',
+        ingredients:["1 cup / 210g thai fragrants rice",
+        "260 ml water",
+        "450g  boneless chicken",
+        "1 chinese sausage (sliced thinly)",
+        "30g grated ginger",
+        "1 tsp Soy sauce",
+        "1/8tsp salt",
+        "1/3 tsp sesame oil",
+        "1/2 tbsp oyster sauce",
+        "1/8 tsp white pepper powder",
+        "1/2 tbsp shaoxing wine",
+        "1/2 tsp sugar",
+        "1tsp corn flour",
+        "1/2tbsp dark soy sauce"
+        ],
+        instructions:["Marinade the chicken for around 1 hour",
+        "Wash rice , soak in water for 1 hour. Drain thoroughly,transfer the rice to claypot, add in 260ml water cover with lid and cook over high heat till boiling , water is almost dried up and small holes start appearing (stir once in a while to prevent stick to the pot)",
+        "Arrange chicken pieces and sausage on the rice, cover with lid and cook over low heat for 20 minutes. Add in seasonings , and continue cook with low heat for another 5 minutes.",
+        "Garnish with chopped spring onion and fried shallots, mix thoroughly and serve",
+    ],
+        youtubeId:"https://www.youtube.com/embed/7fkTdKsY4ck" 
+    },
+    {
+        id: 9,
+        title: "Mutton Soup",
+        author: "KP Kwan",
+        category: "soup",
+        image: url='https://media.istockphoto.com/id/1364381072/photo/shurpa-asian-soup-with-lamb-potatoes-and-carrot-on-white-bowl-on-dark-table.jpg?s=612x612&w=0&k=20&c=DGrMP6a3SEHuZJwPylgVZMYWZRV2Zr4676Ch8Zoc7dM=',
+        ingredients:["750g of mutton with bones",
+        "1.5 liter water",
+        "1 carrot, medium size",
+        "3 stalks celery",
+        "2 medium size tomatoes",
+        "1 tsp cumin powder",
+        "1 tsp coriander powder",
+        "1/2 tsp turmeric powder",
+        "2 tbsp korma powder",
+        "1/2 tsp white pepper powder",
+        "1 tsp garam masala powder",
+        "1.5 tsp salt",
+        "6 bay leaves",
+        ],
+        instructions:["Wash the mutton cubes under running water to remove any debris and blood",
+        "Cut the garlic, ginger, and onion into small pieces and blend these ingredients with water to become a paste",
+        "Boil the mutton and paste and reduce to low heat to simmer the mutton for one hour.",
+        "Remove the scum floating on top with a fine sieve from time to time",
+        "Add the carrots, celery, and tomato and continue simmering for another 30 minutes",
+        "Garnish with more coriander and more fried shallot to serve",
+    ],
+        youtubeId: "https://www.youtube.com/embed/F2bl9cjG6FQ" 
+    },
+    {
+        id: 10,
+        title: "Cendol",
+        author: "Cherry",
+        category: "sides",
+        image: url='https://sm.mashable.com/t/mashable_sea/photo/default/cendolheader_6vzc.1248.jpg',
+        ingredients:["90g corn starch",
+        "4 - 5 fresh or frozen pandan leaves (or pandan extract)",
+        "450ml water",
+        "Optional 1 - 2 drops of green food colouring",
+        "Gula Melaka Syrup",
+        "Coconut Milk",
+        ],
+        instructions:["Cut the pandan into small pieces,blend it into liquid and strain it ",
+        "Mix pandan juice and corn starch and whisk untill no lumps",
+        "Heat and stir the mixture untill it becomes thick paste",
+        "Pour the paste through a strainer with holes into ice water and this is our cendol jellies",
+        "Pour gula melaka syrup,coconut milk and other toppings onto crushed ice",
+        "Finally add the cendol jellies",
+    ],
+        youtubeId: "https://www.youtube.com/embed/S-MtwWEIJYg"
+    },
+    {
+        id: 11,
+        title: "Kuih Lapis",
+        author: "Micheal",
+        category: "sides",
+        image: url='https://i.ytimg.com/vi/jmBlCzT_ooE/maxresdefault.jpg',
+        ingredients:["100g Tapioca Flour",
+        "100g Rice Flour",
+        "1/2 tsp. Salt",
+        "300g Water",
+        "140g Sugar",
+        "200g Coconut Milk",
+        "1 tsp. Pandan Paste",
+        "Food Colouring (optional)"
+        ],
+        instructions:[
+        "Melt the sugar in boiling water and add coconut milk",
+        "Add the mixture to the flour and mix it well (add food colouring)",
+        "Grease the mould and pour the batter into it",
+        "Steam the batter layer by layer for every 3 minutes",
+        "Cut the steamed batter into small pieces",
+        ],
+        youtubeId: "https://www.youtube.com/embed/jmBlCzT_ooE"
+    },
+    {
+        id: 12,
+        title: "Bubur Cha Cha",
+        author: "Jasmyin",
+        category: "sides",
+        image: url='https://storm-asia.com/site/wp-content/uploads/2018/09/IMG_2638_TwoThirds_-Bubur-Cha2_Sept2018-W2.jpg',
+        ingredients:["200g yam (cubed)",
+        "100g purple sweet potato (cubed)",
+        "100g orange sweet potato (cubed)",
+        "100g yellow sweet potato (cubed)",
+        "40g tapioca pearls (sago)",
+        "100g tapioca flour cubes (for bubur cha cha)",
+        "800ml water",
+        "500g coconut milk",
+        "140g rock sugar",
+        "4-5 pandan leaves (knotted)"
+        ],
+        instructions:["Simmer tapioca flour cubes and sago until it cook ",
+        "Steam the cubed potatos untill tender",
+        "Add rock sugar,pandan knots in water and stir untill it dissolves",
+        "Add coconut milk,cubed potatos and mix it well",
+    ],
+        youtubeId: "https://www.youtube.com/embed/uoEXv0xPNk0" 
+    }
+]  ;
+
+// get all recipes
+app.get('/api/recipes', (req, res) => {
+    res.json(recipes);
+  });
+  
+// get recipe by id
+app.get('/api/recipes/:id', (req, res) => {
+  const recipe = recipes.find(r => r.id === parseInt(req.params.id));
+  if (!recipe) return res.status(404).send('Recipe not found');
+  res.json(recipe);
+});
+
+// post a new recipe
+app.post('/api/recipes', (req, res) => {
+    const recipe = {
+      id: recipes.length + 1,
+      title: req.body.title,
+      author: req.body.author,
+      category: req.body.category,
+      image: req.body.image,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions,
+      youtubeId: req.body.youtubeId
+    };
+    recipes.push(recipe);
+    res.status(201).json(recipe);
+  });
+
+// start server
+app.listen(port, () => {
+    console.log(`Recipe API listening at http://localhost:${port}`);
+  });
